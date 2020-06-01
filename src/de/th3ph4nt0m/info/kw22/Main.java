72c56b851dc2ad60274e9283e3fb5902b6ba6026
Th3Ph4nt0m <accounts@th3ph4nt0m.de>
2020-06-01 17:56:50 +0200

    - added main method

---
+++ src/de/th3ph4nt0m/info/kw22/Main.java
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2020 Henrik Steffens aka Th3Ph4nt0m
+ *
+ * Main.java is part of the Informatik
+ * Last edit: 2020.6.1
+ */
+
+package de.th3ph4nt0m.info.kw22;
+
+public class Main
+{
+    public static void main(String[] args){
+        new Simulation(16);
+    }
+}

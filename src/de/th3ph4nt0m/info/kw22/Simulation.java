4541b52b0d5cbc83ffe94394bc5ce534d5a6568f
Th3Ph4nt0m <accounts@th3ph4nt0m.de>
2020-06-01 17:35:21 +0200

    - added method for finding frequency of each element

--- src/de/th3ph4nt0m/info/kw22/Simulation.java
+++ src/de/th3ph4nt0m/info/kw22/Simulation.java
@@ -11,7 +11,7 @@ package de.th3ph4nt0m.info.kw22;
 {
 
     private final int[] results;
-    private int rounds;
+    private final int rounds;
 
     public Simulation(int rounds)
     {
@@ -19,15 +19,47 @@ package de.th3ph4nt0m.info.kw22;
         this.rounds = rounds;
     }
 
-    public void simulate(){
-        for(int i = 0; i<rounds; i++){
-            results[i] = (int) (Math.random()*6+1);
+    public void simulate()
+    {
+        for (int i = 0; i < rounds; i++) {
+            results[i] = (int) (Math.random() * 6 + 1);
         }
     }
 
-    public void printResults(){
-        for(int i = 0; i<rounds; i++){
+    public void printResults()
+    {
+        for (int i = 0; i < rounds; i++) {
             System.out.println("Wurf " + i + ": " + results[i]);
+        }
+    }
+
+    public void countArray()
+    {
+        int[] frequency = new int[6];
+        int visited = -1;
+        for (int i = 0; i < results.length; i++) {
+            int count = 1;
+            for (int j = i + 1; j < results.length; j++) {
+                if (results[i] == results[j]) {
+                    count++;
+                    //To avoid counting same element again
+                    frequency[j] = visited;
+                }
+            }
+            if (frequency[i] != visited) {
+                frequency[i] = count;
+            }
+        }
+
+        //Displays the frequency of each element present in array
+        System.out.println("---------------------------------------");
+        System.out.println(" Element | Frequency");
+        System.out.println("---------------------------------------");
+        for (int i = 0; i < frequency.length; i++) {
+            if (frequency[i] != visited) {
+                System.out.println("    " + results[i] + "    |    " + frequency[i]);
+            }
+            System.out.println("----------------------------------------");
         }
     }
 }
